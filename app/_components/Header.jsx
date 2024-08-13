d69a4b7ac0a81dd0e730819b9fa653b6f2a8e0ae
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-08-13 15:05:20 +0200

    created header components both in sass and .jsx + started styling

---
+++ app/_components/Header.jsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+const Header = () => {
+  const navItems = [
+    { display: "the camp.", slug: "" },
+    { display: "the experience.", slug: "experience" },
+    { display: "the blog.", slug: "blog" },
+  ];
+
+  return (
+    <header className="header">
+      <img className="header__logo" src="/assets/logo.svg" alt="logo" />
+      <ul className="header__nav">
+        {navItems.map((item) => (
+          <li key={item.slug}>
+            <Link href={`/${item.slug}`}>
+              <h5>{item.display}</h5>
+            </Link>
+          </li>
+        ))}{" "}
+      </ul>
+      <Link className="header__button" href="/events">
+        <button className="btn btn--black btn--small">Book Now</button>
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
+
+// To create a normal :
+// function Header() {
+//   return <h2>Header test</h2>;
+// }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main>
-      <h1>Surfcamp Ready !! 🚀🚀🚀🚀</h1>
-    </main>
-  );
+  return <main></main>;
 }
