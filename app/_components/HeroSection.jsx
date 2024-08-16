93c696d648dd4c1a09fc038d0213c0315054b99c
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-08-16 16:30:25 +0200

    created the herosection, with a dynamic theme using props (imgSrc, Headline, Theme color) and logo

--- app/_components/HeroSection.jsx
+++ app/_components/HeroSection.jsx
@@ -1,5 +1,25 @@
-const HeroSection = () => {
-  return <section className="hero"></section>;
+import Link from "next/link";
+
+const HeroSection = ({ imgSrc, headline, theme = "turquoise" }) => {
+  return (
+    <section className="hero">
+      {console.log(`the Hero Section Component has been mounted`)}
+      <div className="hero__background">
+        <img src={imgSrc || "/assets/hero-home.png"} alt="header image" />
+      </div>
+      <div className={`hero__headline hero__headline--${theme}`}>
+        {headline}
+      </div>
+      <button className={`btn btn--medium btn--${theme}`}>
+        <Link href="/events">BOOK NOW</Link>
+      </button>
+      <img
+        className={`hero__logo hero__logo--${theme}`}
+        src="/assets/logo.svg"
+        alt="logo"
+      />
+    </section>
+  );
 };
 
 export default HeroSection;
