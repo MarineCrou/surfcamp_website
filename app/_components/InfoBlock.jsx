9c142566d918912ba4a6307f5bea119da71bb537
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-08-16 18:59:01 +0200

    created the blockInfo component + preped for the info block to have an interchangeable layout (image left or right) using a boolean in the page.jsx object and passing it as a variable, thanks to a data prop in the infoBlocks.jsx function.

---
+++ app/_components/InfoBlock.jsx
@@ -0,0 +1,17 @@
+const InfoBlock = ({ data }) => {
+  const { headline, text, button, reversed } = data;
+  return (
+    <div className={`info  ${reversed ? "info--reversed" : ""}`}>
+      {" "}
+      {/* depending on the reversed boolean, it will apply the class or not */}
+      <img className="info__image" src="./info-blocks/rectangle.png" alt="" />
+      <div className="info__text">
+        <h2 className="info__headline">{headline}</h2>
+        {text}
+        {button}
+      </div>
+    </div>
+  );
+};
+
+export default InfoBlock;
