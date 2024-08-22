295d4a8d0beac8270887ab052376c181b5fa9d4d
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-08-22 14:31:35 +0200

    connected strapi to the next.js front-end, passing the data following the schemas that were created in Strapi as well as the stored data

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
-import axios from "axios";
 import HeroSection from "./_components/HeroSection";
-import InfoBlock from "./_components/InfoBlock";
+import { fetchDataFromStrapi, processInfoBlocks } from "@/utils/strapi.utils";
 
 export default async function Home() {
-  const response = await axios.get(
-    "http://127.0.0.1:1337/api/infoblocks-landing?populate=deep"
-    // change local host (http://localhost:1337/api/infoblocks-landing) into 127.0.0.1: then the server #
-  );
-  console.log(response.data.data.attributes.info_blocks.data);
+  const data = await fetchDataFromStrapi("infoblocks-landing?populate=deep");
+
+  const infoBlockData = processInfoBlocks(data);
+  console.log(infoBlockData);
 
   const heroHeadline = (
     <>
@@ -18,35 +16,11 @@ export default async function Home() {
     </>
   );
 
-  const infoBlockData = {
-    headline: "The experience.",
-    text: (
-      <p className="copy">
-        At Sam’s Surfcamp, we invite you to embark on an unforgettable surfing
-        adventure. Nestled in the heart of [Location] our surf camp offers an
-        exhilarating experience for beginners, intermediate surfers, and
-        seasoned wave riders alike. Dive into the world of surfing with our
-        expert instructors who have years of experience and a deep passion for
-        the sport. Whether you're a first-time surfer looking to catch your
-        first wave or a seasoned pro seeking to enhance your skills, our
-        dedicated team is here to guide you every step of the way. Immerse
-        yourself in the natural beauty of our surf camp's surroundings. Picture
-        yourself waking up to the sound of crashing waves and feeling the warm
-        sand beneath your feet. With pristine beaches and a vibrant coastal
-        atmosphere, [Location] sets the perfect stage for your surf adventure.
-      </p>
-    ),
-    button: (
-      <button className="btn btn--medium btn--turquoise">Book Now</button>
-    ),
-    reversed: false,
-  };
-
   return (
     <main>
       <HeroSection headline={heroHeadline} />
-      <InfoBlock data={infoBlockData} />
-      <InfoBlock data={{ ...infoBlockData, reversed: true }} />
+      {/* <InfoBlock data={infoBlockData} />
+      <InfoBlock data={{ ...infoBlockData, reversed: true }} /> */}
     </main>
   );
 }
---
+++ utils/strapi.utils.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+const BASE_URL = process.env.STRAPI_URL || "http://127.0.0.1:1337/api";
+
+export async function fetchDataFromStrapi(route) {
+  const url = `${BASE_URL}/${route}`;
+
+  try {
+    const response = await axios.get(url);
+    return response.data.data;
+  } catch (err) {
+    throw new Error(`could not fetch data from the ${url}`);
+  }
+}
+
+export function processInfoBlocks(data) {
+  const infoBlocksRaw = data.attributes.info_blocks.data;
+  return infoBlocksRaw.map((infoBlock) => {
+    // console.log(infoBlock.attributes?.image?.data.attributes.url);
+    return {
+      ...infoBlock.attributes, // spreads all properties within that element into a into a new
+      imageSrc: infoBlock.attributes?.image?.data.attributes.url, // creating a new property to Get image URL
+      id: infoBlock.id, // to get the ID
+    };
+  });
+}
+
+// can also be written as follows
+// export function processInfoBlocks(data) {
+//   const infoBlocksRaw = data.attributes.info_blocks.data;
+//   return infoBlocksRaw.map((infoBlock) => ({
+//       ...infoBlock.attributes, // spreads all properties within that element into a into a new
+//       imageSrc: infoBlock.attributes?.image?.data.attributes.url, // creating a new property to Get image URL
+//       id: infoBlock.id, // to get the ID
+//     }));
+// }
