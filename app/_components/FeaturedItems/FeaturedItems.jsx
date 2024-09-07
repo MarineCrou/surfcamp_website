83b3137f95f7d903e9f2fd56d3e563f94b81e53e
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-09-07 19:45:36 +0200

    Made the FeaturedItems component a client component, in order to use the usestate. Point is to display more articles when the  button is clicked. Featuring articles in multiples of 3. Button dispapears if there are less than 3 more articles to display.

--- app/_components/FeaturedItems/FeaturedItems.jsx
+++ app/_components/FeaturedItems/FeaturedItems.jsx
@@ -1,20 +1,36 @@
 // *We're adding the `items` prop, passed in the `feautured articles` from the main blog page.
-
+"use client";
+// everytime you use useState, you're using a client component.
+import { useState } from "react";
 import FeaturedArticle from "./FeaturedArticle";
 
 const FeaturedItems = ({ headline, items }) => {
+  const [itemNumber, setItemNumber] = useState(3);
+  //We want to display 3 more articles, when we click on the see more button, to start off with. Multiples of 3.
+
+  const onShowMore = () => {
+    setItemNumber(itemNumber + 3);
+  };
+
   return (
     <secton className="featured-items">
       <h3 className="featured-items__headline">
         {headline || "Our Featured Articles"}
       </h3>
       <div className="featured-items__container">
-        {items.map((item) => (
+        {/* We're using the slice elements to determine the number of blog articles we want featured in the container. `items.slice(0, itemNumber).map((item) => (...)` means only 3 articles will be displayed, as the usestate of itemNumber = 3 */}
+        {items.slice(0, itemNumber).map((item) => (
           <FeaturedArticle key={item.slug} article={item} />
-          /* The article being passed is the mapped `item`: {items.map((item)*/
+          /* The article being mapped through is `item`: {items.map((item)*/
         ))}
       </div>
-      <button className="btn btn--medium btn--turquoise">See more</button>
+
+      {/* Display the button ONLY if/when there are more articles left to display. If not, hide the button */}
+      {itemNumber < items.length && (
+        <button className="btn btn--medium btn--turquoise" onClick={onShowMore}>
+          See more
+        </button>
+      )}
     </secton>
   );
 };
