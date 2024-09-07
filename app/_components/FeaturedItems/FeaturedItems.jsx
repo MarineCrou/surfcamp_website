615c31000b28cc709d1c9123ee207eb2ca6d5048
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-09-07 12:16:31 +0200

    styled the FeaturedItems and FeaturedArticle components

--- app/_components/FeaturedItems/FeaturedItems.jsx
+++ app/_components/FeaturedItems/FeaturedItems.jsx
@@ -5,7 +5,7 @@ import FeaturedArticle from "./FeaturedArticle";
 const FeaturedItems = ({ headline, items }) => {
   return (
     <secton className="featured-items">
-      <h3 className="featured-items__Headline">
+      <h3 className="featured-items__headline">
         {headline || "Our Featured Articles"}
       </h3>
       <div className="featured-items__container">
