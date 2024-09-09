b5be932054eba92dd3c84d7fd4d3a8deb6790db5
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-09-09 19:31:06 +0200

    created a dynamic version to display the headline, author & time stamp, in either a white or black theme, using the prop article

--- app/_components/Blog/ArticleIntro.jsx
+++ app/_components/Blog/ArticleIntro.jsx
@@ -3,16 +3,26 @@
 import { formatDate } from "@/utils/strapi.utils";
 
 const ArticleIntro = ({ article }) => {
-  console.log(article);
+  console.log("This is the theme:", article.color, "🎉🎉🎉🎉");
+  //   console.log(article);
+
   return (
     <div className="article-intro">
       <div className="article-intro__background">
         <img src={article.featuredImage} alt="" />
       </div>
-      <h3 className="article-intro__headline">{article.headline}</h3>
-      <p className="copy-small bold"> {formatDate(article.publishedAt)} </p>
+      <h3
+        className={`article-intro__headline article-intro__headline--${article.color}`}
+      >
+        {article.headline}
+      </h3>
+      <p className={`copy-small bold copy-small--${article.color}`}>
+        {formatDate(article.publishedAt)}
+      </p>
       {/* We've already created a way to format the date, in the strapi.utils file. So we're just applying that. Don't forge to import it from the file first. */}
-      <p className="copy-small">{article.author}</p>
+      <p className={`copy-small copy-small--${article.color}`}>
+        {article.author}
+      </p>
     </div>
   );
 };
