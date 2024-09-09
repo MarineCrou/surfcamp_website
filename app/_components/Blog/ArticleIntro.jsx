9caf86d516c8c5356ee411378048220ab236e953
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-09-09 16:28:19 +0200

    finished pulling data from strapi using the prop article

--- app/_components/Blog/ArticleIntro.jsx
+++ app/_components/Blog/ArticleIntro.jsx
@@ -1,8 +1,20 @@
 // Getting an article from our parent componnet, which is the blog article componente.
 
+import { formatDate } from "@/utils/strapi.utils";
+
 const ArticleIntro = ({ article }) => {
   console.log(article);
-  return <div className="article-intro"></div>;
+  return (
+    <div className="article-intro">
+      <div className="article-intro__background">
+        <img src={article.featuredImage} alt="" />
+      </div>
+      <h3 className="article-intro__headline">{article.headline}</h3>
+      <p className="copy-small bold"> {formatDate(article.publishedAt)} </p>
+      {/* We've already created a way to format the date, in the strapi.utils file. So we're just applying that. Don't forge to import it from the file first. */}
+      <p className="copy-small">{article.author}</p>
+    </div>
+  );
 };
 
 export default ArticleIntro;
