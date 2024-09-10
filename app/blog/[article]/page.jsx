ff2162478451607a3c2e4bf7356aa72446fa90ab
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-09-10 14:53:48 +0200

    updated the sudocode notes made on the articleIntro.jsx file and main blog>article>page file

--- app/blog/[article]/page.jsx
+++ app/blog/[article]/page.jsx
@@ -1,29 +1,39 @@
-// ! Creating blueprint individual article page, with a dynamically attributed slug : https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+// ! Creating a dynamic page for each individual article based on its slug:
+// https://nextjs.org/docs/app/api-reference/functions/generate-static-params
 
 import ArticleIntro from "@/app/_components/Blog/ArticleIntro";
 import ArticleOverview from "@/app/_components/Blog/ArticleOverview";
 import { fetchBlogArticles, fetchDataFromStrapi } from "@/utils/strapi.utils";
 
-// ? 2. it then gives that slug back to our component (page), through the params. An our component, that is by default serverside, will then fecth the data in strapi to display on the screen
+// ? 2. The slug is passed to this component through params.
+// The component, which is by default server-side rendered, fetches the data from Strapi using the slug to display the article.
 export default async function page({ params }) {
   const { article: slug } = params;
 
-  //   we're fetching all of the articles as this component will be displayed on each indivisual blog article page
+  // Fetch all articles from Strapi. This is necessary because each individual article page
+  // needs the relevant article data based on the dynamically attributed slug.
   const articles = await fetchBlogArticles();
-  const article = articles.find((article) => article.slug === slug);
+  const article = articles.find((article) => article.slug === slug); // Find the article with the matching slug
+
   return (
     <main>
-      {/* The article, is the articles we've fetched in Strapi */}
-      <ArticleIntro article={article} />
-      <ArticleOverview article={article} />
+      {/* Passing the fetched article data to the ArticleIntro and ArticleOverview components */}
+      <section className="article-section">
+        <ArticleIntro article={article} />
+        <ArticleOverview article={article} />
+      </section>
     </main>
   );
 }
 
-// ? 1. generateStaticParams pre-generates a page/component for each one of the articles we've created in Strapi, based on the slug
+// ? 1. generateStaticParams pre-generates static paths for each article page based on the slug.
+// This allows Next.js to create a static page for every article in Strapi, improving performance by pre-rendering pages.
 export async function generateStaticParams() {
-  const articles = await fetchDataFromStrapi("blog-articles"); //   this will give us an array of data, in chich we have the slug displayed
+  // Fetch all blog articles from Strapi to retrieve their slugs
+  const articles = await fetchDataFromStrapi("blog-articles");
+
+  // Map through the articles and return an array of slugs that Next.js will use to generate static pages
   return articles.map((article) => ({
-    article: article.attributes.slug,
+    article: article.attributes.slug, // Return the slug of each article
   }));
 }
